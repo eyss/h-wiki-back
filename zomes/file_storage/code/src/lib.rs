49e9890f2b632650d0b82a1f584460ad1d50e2d1
guillem <guillem.cordoba@gmail.com>
2020-02-28 20:46:54 +0100

    Updated file_storage

--- zomes/file_storage/code/src/lib.rs
+++ zomes/file_storage/code/src/lib.rs
@@ -10,41 +10,28 @@ extern crate serde_json;
 
 use hdk::prelude::*;
 use hdk_proc_macros::zome;
-#[derive(Serialize, Deserialize, Debug, DefaultJson, Clone)]
-pub struct Chunck {
-    chunck: Vec<u8>,
-}
+
 #[derive(Serialize, Deserialize, Debug, DefaultJson, Clone)]
 pub struct File {
-    chuncks: Vec<Address>,
     name: String,
-    agent_address: Address,
+    size: u64,
+    r#type: String,
+    last_modified: u64,
+    chunks: Vec<Address>,
 }
+
 #[derive(Serialize, Deserialize, Debug, DefaultJson, Clone)]
 pub struct File2 {
     file: Vec<u8>,
     name: String,
 }
-impl Chunck {
-    pub fn from(chunck: Vec<u8>) -> Chunck {
-        Chunck { chunck }
-    }
-    pub fn entry(self) -> Entry {
-        Entry::App("chunck".into(), self.into())
-    }
-}
+
 impl File {
-    pub fn from(chuncks: Vec<Address>, name: String) -> File {
-        File {
-            chuncks,
-            name,
-            agent_address: hdk::api::AGENT_ADDRESS.clone(),
-        }
-    }
     pub fn entry(self) -> Entry {
         Entry::App("file".into(), self.into())
     }
 }
+
 #[zome]
 mod file_storage {
     #[init]
@@ -72,31 +59,25 @@ mod file_storage {
     #[entry_def]
     pub fn chunk_def() -> ValidatingEntryType {
         entry!(
-            name: "chunck",
+            name: "chunk",
             description: "this is an entry representing some profile info for an agent",
             sharing: Sharing::Public,
             validation_package: || {
                 hdk::ValidationPackageDefinition::Entry
             },
-            validation: | _validation_data: hdk::EntryValidationData<Chunck>| {
+            validation: | _validation_data: hdk::EntryValidationData<String>| {
                Ok(())
             }
         )
     }
-    fn create_if_exist(entry: Entry, address: Address) -> ZomeApiResult<Address> {
-        if let Ok(Some(_)) = hdk::get_entry(&address) {
-            Ok(address)
-        } else {
-            hdk::commit_entry(&entry)
-        }
-    }
+    /*
     #[zome_fn("hc_public")]
     fn create_file(file: Vec<u8>, name: String) -> ZomeApiResult<Address> {
         hdk::commit_entry(
             &File::from(
                 file.chunks(32000)
-                    .map(|chunck| {
-                        let entry = Chunck::from(chunck.to_vec()).entry();
+                    .map(|chunk| {
+                        let entry = Chunk::from(chunk.to_vec()).entry();
                         create_if_exist(entry.clone(), entry.address())
                     })
                     .filter_map(Result::ok)
@@ -105,32 +86,44 @@ mod file_storage {
             )
             .entry(),
         )
-    }
+    } 
     #[zome_fn("hc_public")]
     fn get_file(address: Address) -> ZomeApiResult<File2> {
         let file: File = hdk::utils::get_as_type(address)?;
         Ok(File2 {
             file: file
-                .chuncks
-                .into_iter()
-                .map(|chunck_address| hdk::utils::get_as_type::<Chunck>(chunck_address))
-                .filter_map(Result::ok)
-                .map(|chunck| chunck.chunck)
-                .collect::<Vec<Vec<u8>>>()
-                .concat(),
+            .chunks
+            .into_iter()
+            .map(|chunk_address| hdk::utils::get_as_type::<Chunk>(chunk_address))
+            .filter_map(Result::ok)
+            .map(|chunk| chunk.chunk)
+            .collect::<Vec<Vec<u8>>>()
+            .concat(),
             name: file.name,
         })
     }
+    */
     #[zome_fn("hc_public")]
-    fn create_chunck(chunck: Vec<u8>) -> ZomeApiResult<Address> {
-        let entry = Chunck::from(chunck).entry();
+    fn get_entry(address: Address) -> ZomeApiResult<Option<Entry>> {
+        hdk::get_entry(&address)
+    }
+    #[zome_fn("hc_public")]
+    fn create_chunk(chunk: String) -> ZomeApiResult<Address> {
+        let entry = Entry::App("chunk".into(), JsonString::from_json(chunk.as_str()));
         create_if_exist(entry.clone(), entry.address())
     }
     #[zome_fn("hc_public")]
-    fn get_chunck(address: Address) -> ZomeApiResult<Chunck> {
-        hdk::utils::get_as_type(address)
+    fn create_file(file: File) -> ZomeApiResult<Address> {
+        let entry = file.entry();
+        hdk::commit_entry(&entry)
+    }
+
+}
+
+fn create_if_exist(entry: Entry, address: Address) -> ZomeApiResult<Address> {
+    if let Ok(Some(_)) = hdk::get_entry(&address) {
+        Ok(address)
+    } else {
+        hdk::commit_entry(&entry)
     }
 }
-//[network]
-//sim2h_url = 'ws://public.sim2h.net:9000'
-//type = 'sim2h'
